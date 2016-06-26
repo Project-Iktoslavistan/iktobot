30a7138e6adc143005f456f13b1c09fcfa299f25
thewatcher <thewatcheriswatchinu@gmail.com>
2016-06-26 02:14:45 +0200

    code that should work but doesn't

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var loginDetails = require("./login.json");
+var request = require("request");
 var DiscordClient = require('discord.io');
 var bot = new DiscordClient({
     autorun: true,
@@ -16,6 +17,30 @@ bot.on("message", function(user, userID, channelID, message, server) {
         bot.sendMessage({
             to: channelID,
             message: "hi"
+        });
+    }
+    else if (message.split(" ")[0] === prefix + "poll") {
+        var formData = {
+            title: "This is a test poll.",
+            options: [
+                "Option #1",
+                "Option #2"
+            ],
+            multi: true
+        }
+        var options = {
+            url: "http://strawpoll.me/api/v2/polls",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            form: formData
+        }
+        request.post(options, function(err, httpResponse, body){
+            console.log(httpResponse);
+        });
+        bot.sendMessage({
+            to: channelID,
+            message: "polling..."
         });
     }
 });
